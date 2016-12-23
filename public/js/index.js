2dcc13b7c49a3337be3912cf581abdd5a518677e
Song Ji <songji0509@gmail.com>
2016-12-23 11:42:44 -0800

    Complete button animation setup

--- public/js/index.js
+++ public/js/index.js
@@ -2,8 +2,18 @@
    window.addEventListener('keydown', function(e){
      console.log(e.keyCode);
      const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
      if (!audio) return; // Stop the function
      audio.currentTime = 0; // rewind to the start of sound
      audio.play();
+     key.classList.add('playing');
    })
+   
+   const keys = document.querySelectorAll('.key');
+   keys.forEach( key => key.addEventListener('transitionend', function(e){
+     if(e.propertyName !== 'transform') return;
+     this.classList.remove('playing');
+   }));
+   
 })();
+
