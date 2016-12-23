afed8d9adc5c79cbf2adc4fb575a6d9f0b5697f3
Song Ji <songji0509@gmail.com>
2016-12-23 11:46:29 -0800

    Orgnize code

--- public/js/index.js
+++ public/js/index.js
@@ -1,19 +1,21 @@
 (function() {
-   window.addEventListener('keydown', function(e){
-     console.log(e.keyCode);
+   function playSound(e){
      const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
      const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
      if (!audio) return; // Stop the function
      audio.currentTime = 0; // rewind to the start of sound
      audio.play();
      key.classList.add('playing');
-   })
+   }
    
-   const keys = document.querySelectorAll('.key');
-   keys.forEach( key => key.addEventListener('transitionend', function(e){
+   function removeTransition(e){
      if(e.propertyName !== 'transform') return;
      this.classList.remove('playing');
-   }));
+   }
    
+   const keys = document.querySelectorAll('.key');
+   keys.forEach( key => key.addEventListener('transitionend', removeTransition));
+   
+   window.addEventListener('keydown', playSound);
 })();
 
